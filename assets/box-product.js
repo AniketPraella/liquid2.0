737322c30cdd3536af39af2b0e89b761e71b4d96
AniketPraella <aniket@praella.com>
2022-11-16 17:34:29 +0530

    Bug Fix

--- assets/box-product.js
+++ assets/box-product.js
@@ -1,1 +1,29 @@
-productarray = [];
+
+
+// multi product add
+var addcartmultiproduct = (e) => {
+    e.preventDefault();
+    var request = new XMLHttpRequest();
+    var url = "/cart/add.js";
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+        var jsonData = JSON.parse(request.response);
+        console.log(jsonData);
+        
+        console.log("product array - " + productarray)
+        
+        }
+    };
+    pid = document.querySelector(".bundle-box-product-page-right-sec [name='id']").value
+    pqty = document.querySelector(".bundle-box-product-page-right-sec [name='quantity']").value
+
+    var data = JSON.stringify({"id": pid, "quantity": pqty});
+
+    console.log("data " + data);
+    request.send(data);
+    productarray.splice(0, productarray.length);
+    updatebundleproduct();
+    addproducttobundleempty();
+}
