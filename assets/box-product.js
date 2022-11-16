ad6470e66abe19af703bb29997c27e88e4240440
AniketPraella <aniket@praella.com>
2022-11-16 19:01:54 +0530

    working on sub product

--- assets/box-product.js
+++ assets/box-product.js
@@ -3,6 +3,9 @@
 // multi product add
 var addcartmultiproduct = (e) => {
     e.preventDefault();
+
+    /******************************************* for main product *********************************************/ 
+
     var request = new XMLHttpRequest();
     var url = "/cart/add.js";
     request.open("POST", url, true);
@@ -41,6 +44,49 @@ var addcartmultiproduct = (e) => {
 
     console.log("data " + data);
     request.send(data);
+    /******************************************* for main product END *********************************************/ 
+
+    /******************************************* for sub products *********************************************/ 
+
+    /*
+    for (subp = 0; subp < productarray.length; subp++) {
+        console.log('subpval ' + (subp+1));
+        
+        var request2 = new XMLHttpRequest();
+        var url2 = "/cart/add.js";
+        request.open("POST", url2, true);
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function() {
+            if (request2.readyState === 4 && request2.status === 200) {
+            var jsonData2 = JSON.parse(request2.response);
+            console.log(jsonData2);
+                    
+            }
+        };
+
+        productarray[subp].variant_image
+
+        pid2 = document.querySelector(".bundle-box-product-page-right-sec [name='id']").value
+        pqty2 = document.querySelector(".bundle-box-product-page-right-sec [name='quantity']").value
+
+        var data2 = JSON.stringify({
+            "id": productarray[subp].variant_id,
+            "quantity": productarray[subp].quantity,
+            properties: {
+                "randomnumber": randomnumber,
+                "type": "sub_product"
+            }
+        });
+
+        console.log("data " + data2);
+        request.send(data2);
+
+        }
+        */
+
+        /******************************************* for sub products END *********************************************/ 
+
+
     productarray.splice(0, productarray.length);
     updatebundleproduct();
     addproducttobundleempty();
