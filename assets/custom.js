644bf33820ceb80fc2107dce42ea2019e2de735d
AniketPraella <aniket@praella.com>
2022-11-24 14:57:03 +0530

    new collection template and quick view

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,4 @@
+/*
 const customSelectors = {};
 class customJS {
     constructor() {
@@ -5,3 +6,40 @@ class customJS {
 }
 
 typeof customJS !== 'undefined' && new customJS();
+*/
+
+let quickshopLayout2 =  document.querySelectorAll('.quickshop-layout2') || null;
+if (quickshopLayout2 != null){
+    quickshopLayout2.forEach(quickbtn => quickbtn.addEventListener('click', (event)=>{
+        event.preventDefault();
+        phandleelement = event.currentTarget;
+        console.log('phandleelement', phandleelement);
+        phandle = phandleelement.getAttribute('data-handle');
+        console.log('phandle', phandle);
+        loadquickshopLayout2(phandle).then(quickShopDataLayout2 =>{
+            targetquickshop = document.querySelector('quick-shop');
+            targetquickshop.innerHTML = quickShopDataLayout2;
+            siteOverlay.prototype.showOverlay();
+            document.querySelector('quick-shop .modal').classList.add('open');
+            closequickshopl2 = document.querySelector('quick-shop .close-quickshop') || null;
+            closequickshopfun(closequickshopl2);
+        });
+    }
+    ));
+
+    function closequickshopfun(closequickshopl2){
+        closequickshopl2.addEventListener('click', (event)=>{
+            event.preventDefault();
+            targetquickshop = document.querySelector('quick-shop');
+            targetquickshop.innerHTML = '';
+            siteOverlay.prototype.hideOverlay();
+        })
+    }
+}
+
+async function loadquickshopLayout2(handle){
+    let requestURL = `/products/${handle}?view=quickshop&sections=template-product-quickshop`;
+    const response = await fetch(requestURL);
+    const quickShopDataLayout2 = await response.json();
+    return quickShopDataLayout2['template-product-quickshop'];
+}
